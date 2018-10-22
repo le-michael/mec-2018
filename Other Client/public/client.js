5ad7938e5048ceb6241a219c8037408be5183a90
Khang Nguyen <nnmkhang166@gmail.com>
2018-10-22 17:36:11 -0400

    Fixed time
    - need to fix UI
    - Need To start log on Text file
    - Need to read and write from the text File
    -Write to the textfile and re-read from the textfile

--- Other Client/public/client.js
+++ Other Client/public/client.js
@@ -3,62 +3,28 @@ var socket = io.connect();
 
 var months = ["January","Feburary","March","April","May","June","July","Agust","September","October"
 ,"November","December"];
-var btn = document.getElementById("send")
-var message = document.getElementById("message");
-/*
-btn.addEventListener("click",function(){
 
 
-
-    talk(message);
-    message.value = ""
-    console.log("button press");
-});
-
-*/
 
 socket.on("chat",function(data){ // not sure why it plays twice but we'll figure that out 
-    
+    var msg = new SpeechSynthesisUtterance(data.message);
+    window.speechSynthesis.speak(msg);
 
-var msg = new SpeechSynthesisUtterance(data.message);
-window.speechSynthesis.speak(msg);
+    var d = new Date();
 
+    var h = d.getHours(); // 24 hour clock in est 
 
-var d = new Date();
-var h = d.getUTCHours() - 16;
-var m = d.getUTCMinutes();
-var s = d.getUTCSeconds(); 
-var time = h.toString()+":"+m.toString()+":"+s.toString();
-console.log(time);
+    console.log(h);
 
-output.innerHTML += "<p>" + data.message+ "<span align = 'right'>"  +time+ "</span>"+ "</p>" ;
+    var m = d.getUTCMinutes();
+    var s = d.getUTCSeconds(); 
+    var time = h.toString()+":"+m.toString()+":"+s.toString();
+    console.log(time);
+
+    output.innerHTML += "<p>" + data.message+ "<span align = 'right'>"  +time+ "</span>"+ "</p>" ;
 
 });
 
 
 // FUNCTIONS FOR LE 
-/*
-function talk(text){ // takes in the string, outputs the text to speech 
-    var d = new Date();
-    var h = d.getUTCHours() - 16;
-    var m = d.getUTCMinutes();
-    var s = d.getUTCSeconds(); 
-    console.log(day,months[month])
-
-    date = months[month]+ " " +day.toString();
-
-    
-    var time = h.toString()+":"+m.toString()+":"+s.toString();
-    console.log(time);
-
-
-    socket.emit("chat",{
-        message : text.value,
-        time : time,
-        date :date
-    });
-}
-
-
-*/
 
