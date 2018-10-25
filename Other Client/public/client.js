32cee673fc7fbdfd0789a69a12ea29923033962b
Khang Nguyen <nnmkhang166@gmail.com>
2018-10-25 17:37:57 -0400

    finished chatlog

--- Other Client/public/client.js
+++ Other Client/public/client.js
@@ -1,27 +1,17 @@
-var socket = io.connect(); 
+var socket = io.connect('http://localhost:5000'); 
 var fs; 
 // reading file 
 
+socket.on("update",function(data){ 
+    console.log("recived from update function",data);
+    output.innerHTML = data;
+});
 
-
-
-var months = ["January","Feburary","March","April","May","June","July","Agust","September","October"
-,"November","December"];
-
-socket.on("clientchat",function(data){ // not sure why it plays twice but we'll figure that out 
+socket.on("clientchat",function(data){ 
     console.log(data);
     var msg = new SpeechSynthesisUtterance(data.message);
     window.speechSynthesis.speak(msg);
 
-   
-
-    output.innerHTML += "<p>" + data.time+ " - "+"<span align = 'right'>"  +data.message+ "</span>"+ "</p>" ; //  adds message to the current HTML 
-
-});
-// initial update function happens when the server is connected, should update everytime there is a refresh
-socket.on("update",function(data){ 
-    console.log("recived from update function",data);
-    output.innerHTML = data;
-
+    output.innerHTML += "<p>" + data.date + " At " + data.time+ " - "+"<span align = 'right'>"  +data.message+ "</span>"+ "</p>" ; //  adds message to the current HTML 
 
 });
--- Other Client/server.js
+++ Other Client/server.js
@@ -3,71 +3,69 @@ var express = require('express'); // using express for dependancies
 var app = express();
 var socket = require('socket.io');
 var fs = require('fs');
-
-var lines = fs.readFileSync('./chatlog.txt', 'utf-8') // read file synchronosly since the textfile should load before the user sees the screen
-    .split('\r\n') // change to \n later 
-    .filter(Boolean);
-console.log(lines);
+app.use(express.static('public'));
 
 
-
-var outputStr = "";
-
-
-
-/*
-// initial conversion from text to output html 
-for( var i = 0; i < lines.length; i++){
-    outputStr += "<p>" +  lines[i] + "</p>" ;  
-
-}
-*/
-
-// forloop with sychronous loops so that the tagging happens at the right time, asychonously messes up the order of the <p> tags 
-
-lines.forEach(item => {
-    outputStr += "<p>"+item+"</p>";
-});
-
-console.log(outputStr,"after initial load type: ", typeof outputStr); // outputStr is a string with  < p > tags. 
-// pass in the text file as a string into the
-
-// app settings 
+// server settings 
 var server = app.listen(5000, "0.0.0.0",function(){ // opes the port
     console.log("lisetning to requests on port 5000")
 });
-app.use(express.static('public'));
+
 var io = socket(server); 
+
 
 // socket functions // 
 io.on("connection",function(socket){ // if connected 
     console.log("made socket connection",socket.id);
 
-    console.log('emiiting',outputStr);
-    socket.emit("update",outputStr);
-
-
+    socket.emit("update",loadFile()); // reload the file every time the socket connection is made
 
     socket.on("chat",function(data){ // chat comes from react server 
         console.log(data.message,"this is the data comming from the chat function");
 
-
         var clientMessage = {
             message : data.message,
-            time: getTime()
+            time: getTime(),
+            date : getDayMonth()
         };
         console.log(clientMessage);
         socket.broadcast.emit("clientchat", clientMessage); // all sockets connected clientchat sends to the client side 
-
-        fs.appendFileSync('./chatlog.txt', clientMessage.time+clientMessage.message +"\r\n");
+        var text = clientMessage.date + " At "+clientMessage.time + " - " + clientMessage.message;
+        writeToFile(text);
     });
 });
 
-function getTime(){
-    var d = new Date();
-    var h = d.getHours(); // 24 hour clock in est 
-    var m = d.getUTCMinutes();
-    var s = d.getUTCSeconds(); 
-    return h.toString()+":"+m.toString()+":"+s.toString();
 
+// functions // 
+function getTime(){ // gets the time when the message is sent
+    var d = new Date();
+    return d.getHours().toString() + ":" +d.getMinutes().toString() + ":" + d.getSeconds().toString();
+}
+
+function getDayMonth(){ // gets the date that the message was sent 
+    var months = ["January","Feburary","March","April","May","June","July","Agust","September","October","November","December"];
+    var d = new Date(); 
+    return months[d.getMonth()] + " " + d.getDate();
+}
+
+function writeToFile(text){ // writes a string to the file in the system
+    text  = text +'\r\n';
+    fs.appendFile('./chatlog.txt',text);
+}
+
+function loadFile(){ // loads the file and returs a string with the <p> tags included. 
+
+    var outputStr = ""; 
+
+    //read file synchronosly
+    var lines = fs.readFileSync('./chatlog.txt', 'utf-8') // read file synchronosly since the textfile should load before the user sees the screen
+    .split('\r\n') // change to \n later 
+    .filter(Boolean);
+    
+    // convert array to string with <p> tags 
+    lines.forEach(item => {
+    outputStr += "<p>"+item+"</p>";
+    });
+
+    return outputStr;
 }
